53c2b96c861b56322ad1a305ab1b7bc81a0eb74e
ADITHYA-RITHAMBARA <98386259+ADITHYA-RITHAMBARA@users.noreply.github.com>
2024-08-10 22:21:21 +0530

    Evenorodd.java

---
+++ Evenorodd.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Evenorodd
+{
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int num=sc.nextInt();
+    if(num==0)
+    {
+        System.out.println(num+" is Zero" );
+    }
+    else if(num%2==0)
+    {
+        System.out.println(num+" is Even" );
+    }
+    else
+    {
+        System.out.println(num+" is Odd");
+    }
+    
+  
+}    
+    
+}
