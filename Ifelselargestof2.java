5b537b7c06897b9d7827b8578a63945f2ac99ec5
ADITHYA-RITHAMBARA <98386259+ADITHYA-RITHAMBARA@users.noreply.github.com>
2024-08-10 21:56:52 +0530

    Ifelselargestof2.java

---
+++ Ifelselargestof2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Ifelselargestof2
+{
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter first number: ");
+    int a=sc.nextInt();
+    System.out.println("Enter second number: ");
+    int b=sc.nextInt();
+    if(a>b)
+    {
+        System.out.println(a+" is greater than "+b);
+    }
+    else if(a==b)
+    {
+        System.out.println("They are equal");
+    }
+    else
+    {
+        System.out.println(b+" is greater than "+a);
+    }
+    
+  
+}    
+    
+}
